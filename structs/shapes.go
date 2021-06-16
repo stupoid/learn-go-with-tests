c7b082a0d6213248422a3abff59c4d8ebd970da2
Kelvin <there.are.no.mails@gmail.com>
2021-06-16 22:27:32 +0800

    Add Perimeter to Shape interface

--- structs/shapes.go
+++ structs/shapes.go
@@ -4,6 +4,7 @@ import "math"
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -15,6 +16,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,7 +28,12 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Triangle struct {
+	// Right Angled
 	Base   float64
 	Height float64
 }
@@ -32,6 +42,6 @@ func (t Triangle) Area() float64 {
 	return t.Base * t.Height * 0.5
 }
 
-func Perimeter(r Rectangle) float64 {
-	return 2 * (r.Width + r.Height)
+func (t Triangle) Perimeter() float64 {
+	return t.Base + t.Height + math.Sqrt(math.Pow(t.Base, 2)+math.Pow(t.Height, 2))
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -3,12 +3,23 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
+	perimeterTests := []struct {
+		name         string
+		shape        Shape
+		hasPerimeter float64
+	}{
+		{name: "Rectangle", shape: Rectangle{10.0, 10.0}, hasPerimeter: 40.0},
+		{name: "Circle", shape: Circle{10.0}, hasPerimeter: 62.83185307179586},
+		{name: "Triangle", shape: Triangle{12.0, 6.0}, hasPerimeter: 31.41640786499874},
+	}
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	for _, tt := range perimeterTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Perimeter()
+			if got != tt.hasPerimeter {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+			}
+		})
 	}
 }
 
